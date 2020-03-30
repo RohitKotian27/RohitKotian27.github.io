da308559ba5b13f7889d37201139470206fc5fa6
RohitKotian27 <43231307+RohitKotian27@users.noreply.github.com>
2020-03-31 00:00:56 +0530

    Add files via upload

---
+++ To-Do-List/js/myScript.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.myform').on('click','.myBtn',function(){
+		var myValue = $('.inp1').val();
+		$('.inp1').empty();
+		if(myValue == ""){
+			alert("This field can't be kept empty!");
+			$('.inp1').focus();
+		}
+		else{
+			var result = '<div class="mytext">\
+						<div class="remove">X</div>' + myValue + '</div>';
+		$('.myList').append(result);
+		$('.inp1').focus();
+	}
+	});
+	$('.myList').on('click','.remove',function(){
+		$(this).parent().fadeOut(1000);
+	});
+});
