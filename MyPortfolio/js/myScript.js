e4cbaf0f756db343fe3406b249c46515a541ea89
Rohit Kotian <43231307+RohitKotian27@users.noreply.github.com>
2020-04-11 13:18:23 +0530

    Update myScript.js

--- MyPortfolio/js/myScript.js
+++ MyPortfolio/js/myScript.js
@@ -8,4 +8,11 @@ $(document).ready(function(){
 		}
 	});
 	$('.customText').trigger('change');
+
+	/*Accordian JS Starts here*/
+	$(".btn_down").click(function(){
+		$(this).toggleClass("trnsfrm_180");
+		$(this).parent().next().slideToggle(500);
+	});
+	/*Accordian JS ends here*/
 });
