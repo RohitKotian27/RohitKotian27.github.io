414c6cf1b8a5e5f53102cea6963d910756b38831
Rohit Kotian <43231307+RohitKotian27@users.noreply.github.com>
2020-05-02 13:04:01 +0530

    Update myScript.js

--- To-Do-List/js/myScript.js
+++ To-Do-List/js/myScript.js
@@ -1,24 +1,23 @@
 $(document).ready(function(){
 	$('.myform').on('click','.myBtn',function(){
 		var myValue = $('.inp1').val();
-		$('.inp1').empty();
 		if(myValue == ""){
 			alert("This field can't be kept empty!");
 			$('.inp1').focus();
 		}
-		else{
+		else{ 	
 			var result = '<div class="mytext">\
-						<div class="fa fa-trash-o"></div>' + myValue + '</div>';
+						<div class="fa fa-trash-o del_btn"></div>' + myValue + '</div>';
 		    $('.myList').append(result);
 		    $('.inp1').val("");
 	}
 	});
 
-	$('.myList').on('click','.fa-trash-o',function(){
+	$('.myList').on('click','.del_btn',function(){
 		$(this).parent().fadeOut(1000);
 	});
 
-    $('.myList').on('click','.mytext',function(){
+    $('.myList').on('dblclick','.mytext',function(){
     	$(this).addClass("completed");
     	$(this).children().css("color","#fff");
     });
