00d8e0abb6439d4726462d64896b937881b8c280
Rohit Kotian <43231307+RohitKotian27@users.noreply.github.com>
2021-04-26 20:42:32 +0530

    Update myScript.js

--- Random_Quote_Generator/js/myScript.js
+++ Random_Quote_Generator/js/myScript.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
 	let randomValue;
+	let copiedValue;
 	$('.custom_btn').click(function(){
 		fetch('https://api.quotable.io/random')
 	  .then(response => response.json())
 	  .then(data => {
 			randomValue = data.content;
+			$("#quote").show();
 	    $('#quote').html('"' +  randomValue + '"');
+			$(".add_btns").show();
+			$("#myInput").val(randomValue);
+			});
 	  });
+
+		// Copy Button code goes here
+		$("#myBtn").click(function(){
+				//To select the text
+				alert("Text has been copied");
+				$("#myInput").attr("type","text");
+				$("#myInput").select();
+				document.execCommand("Copy");
+				$("#myInput").attr("type","hidden");
+		});
 	});
-});
