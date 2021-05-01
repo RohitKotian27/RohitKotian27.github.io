5c6493e23de8a45151e38ae7e51c368e81c5e3c1
Rohit Kotian <43231307+RohitKotian27@users.noreply.github.com>
2021-05-01 20:47:41 +0530

    Update myScript.js

--- instagram-post-creator/js/myScript.js
+++ instagram-post-creator/js/myScript.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
 	let randomValue;
-	let copiedValue;
-	let count = 1;
 	$('.custom_btn').click(function(){
+
 		fetch('https://api.quotable.io/random')
 	  .then(response => response.json())
 	  .then(data => {
 			randomValue = data.content;
-			count += 1;
-			var url = `https://picsum.photos/280/280?&random=${count}`
-			$(".custm_img").attr("src",url);
-	    $('#quote').html('"' +  randomValue + '"');
-			$("#quote").show();
-			// $(".add_btns").show();
+			fetch(`https://source.unsplash.com/280x280/?nature`).then((response) => {
+    	  var url = `${response.url}`;
+				$(".custm_img").attr("src",url);
+				$('#quote').html('"' +  randomValue + '"');
+			 	$("#quote").show();
+			 	$(".add_btns").show();
+  		});
 			});
+
 	  });
+
+		$("#myBtn").click(function(){
+			domtoimage.toPng(document.getElementById('post_section'), { quality: 1.0 })
+    .then(function (dataUrl) {
+        var link = document.createElement('a');
+        link.download = 'my-post.png';
+        link.href = dataUrl;
+        link.click();
+    });
+	});
 	});
