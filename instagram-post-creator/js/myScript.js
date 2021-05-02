1fd9bc80fa73a56a0f0a27492a1900054c1d9b24
Rohit Kotian <43231307+RohitKotian27@users.noreply.github.com>
2021-05-02 12:27:20 +0530

    Update myScript.js

--- instagram-post-creator/js/myScript.js
+++ instagram-post-creator/js/myScript.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	  .then(response => response.json())
 	  .then(data => {
 			randomValue = data.content;
-			fetch(`https://source.unsplash.com/280x280/?nature`).then((response) => {
+			fetch(`https://source.unsplash.com/600x600/?nature`).then((response) => {
     	  var url = `${response.url}`;
 				$(".custm_img").attr("src",url);
 				$('#quote').html('"' +  randomValue + '"');
@@ -18,12 +18,32 @@ $(document).ready(function(){
 	  });
 
 		$("#myBtn").click(function(){
-			domtoimage.toPng(document.getElementById('post_section'), { quality: 1.0 })
-    .then(function (dataUrl) {
-        var link = document.createElement('a');
-        link.download = 'my-post.png';
-        link.href = dataUrl;
-        link.click();
-    });
+		const node = document.getElementById("post_section");
+		const scale = 600 / node.offsetWidth;
+		this.shot_loading = true;
+
+		domtoimage
+		.toPng(node, {
+		    height: node.offsetHeight * scale,
+		    width: node.offsetWidth * scale,
+		    style: {
+		    transform: "scale(" + scale + ")",
+		    transformOrigin: "top left",
+		    width: node.offsetWidth + "px",
+		    height: node.offsetHeight + "px"
+		    }
+		})
+		.then(dataUrl => {
+		    this.baseData = dataUrl;
+		    this.shot_loading = false;
+				var link = document.createElement('a');
+		    link.download = 'my-post.png';
+		    link.href = dataUrl;
+		    link.click();
+		})
+		.catch(error => {
+		    this.shot_loading = false;
+		    console.error("oops, something went wrong!", error);
+		});
 	});
 	});
